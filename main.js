39445c15aa92725ed2ce4cfecacaebda2324ad06
Serg523 <46010173+Serg523@users.noreply.github.com>
2020-07-17 00:33:44 +0300

    Fixed

--- main.js
+++ main.js
@@ -1,92 +1,62 @@
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+let clipboard = new ClipboardJS('.indigo');
+let push = document.querySelectorAll('.indigo');
+let ph = document.querySelector('#push');
+let tb = document.querySelector('#testBoard');
+for (let i = 0; i < push.length; i++) {
+  push[i].onclick = function () {
+    if (!ph) {
+        let el = document.createElement('div');
+        el.id = 'push';
+        el.innerText = 'Скопированно в буфер обмена';
+        document.body.appendChild(el);
+        setTimeout(
+          function() {
+            el.remove();
+          },
+          2000
+        );
+    }
+  }
 }
-html {
-  scroll-behavior: smooth;
+let cp;
+clipboard.on('success', function(e) {
+  cp = e.text;
+  e.clearSelection();
+});
+
+let pastBtn = document.querySelector('.past');
+
+pastBtn.onclick = function () {
+  if (cp == undefined) {
+    tb.value = 'TEST';
+  }else {
+    if ((cp == '#ffffff') || (cp == '#f5f5f5') || (cp == '#FFFF00')) {
+      tb.style.color = '#000000';
+      tb.value = cp;
+    }else {
+      tb.style.color = '#ffffff';
+      tb.value = cp;
+    }
+    
+  }
+  tb.style.backgroundColor = cp;
 }
-body {
-    font-size: 15pt;
-    font-family: 'Comfortaa';
-    background: #202833;
+
+let clr = document.querySelector('.clr');
+
+clr.onclick = function () {
+  tb.value = 'TEST';
+  tb.style.backgroundColor = '#202833';
+  tb.style.color = '#ffffff';
 }
-#aboutCont {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0,0,0,0.4);
-  display: none;
-  justify-content: center;
-  align-items: center;
+
+let about = document.querySelector('#aboutCont');
+let aboutBtn = document.querySelector('.abActive');
+let open = document.querySelector('.openActive');
+
+open.onclick = function () {
+  about.classList.toggle('active');
 }
-#aboutCont.active {
-  display: flex;
-}
-.aboutMe {
-  width: 60vw;
-  height: 40vh;
-  background: #202833;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  box-shadow: 0 0 10px rgba(0,0,0,0.75);
-}
-.aboutMe h3 {
-  font-size: 3.5vh;
-  font-weight: bold;
-}
-.dev {
-  color: #fff;
-  letter-spacing: 2px;
-  font-size: 2.5vh;
-}
-.aboutMe button {
-  width: 15vw;
-  height: 7vh;
-  border: none;
-  color: #fff;
-  font-size: 2vh;
-  outline: none;
-  border-radius: 5px;
-  background: #024b54;
-  letter-spacing: 2px;
-}
-.header {
-  width: 100vw;
-  height: 10vh;
-   background: #083B6F;
-   box-shadow: 0 0 10px rgba(0,0,0,0.75);
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
- 
-}
-h1 {
-  letter-spacing: 5px;
-  font-weight: lighter;
-  color: #fff;
-  margin-left: 20px;
-  font-size: 2.8vh;
- 
-}
-.header button {
-  margin-right: 20px;
-  background: none;
-  border: none;
-  outline: none;
-  color: #fff;
-  font-size: 2.8vh;
-}
-.test {
-  width: 95vw;
-  height: 10vh;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+aboutBtn.onclick = function () {
+  about.classList.remove('active');
 }
